2e86e0917302b07954680defb8cf8460acdc3e30
AravindMurugan <mi.arvindmurugan@gmail.com>
2021-04-26 13:19:32 +0530

    cakeContainer-redux

---
+++ src/App3.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CakeContainer from "./components2/CakeContainer";
+import "./styles.css";
+
+function App3() {
+  return (
+    <div>
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App3;
---
+++ src/components2/CakeContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CakeContainer() {
+  return (
+    <div>
+      <h1>CakeContainer</h1>
+    </div>
+  );
+}
+
+export default CakeContainer;
